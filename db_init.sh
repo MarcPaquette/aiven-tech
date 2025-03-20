2cd92ea12f0eb2dd16c12d3269e9c238535e33ba
Marc Paquette <marc.paquette@broadcom.com>
2025-03-20 16:05:08 +0000

    Get connectors up and running

--- db_init.sh
+++ db_init.sh
@@ -2,13 +2,14 @@
 set -ex
 
 # Wait for the service to be up
-avn service wait demo-pg
+avn service wait demo-postgres
 
 # Get the connection string
-DB_SERVICE_URI=$(avn service get demo-pg --format '{service_uri_params}')
+DB_SERVICE_URI=$(avn service get demo-postgres --format '{service_uri}')
 
 # Initialize database publications, tables & data
 psql "$DB_SERVICE_URI" -f ./db_init.sql
 
-# Restart the service
-# avn 
+# Restart the connector
+avn service connector restart franz kafka-pg-connector
+avn service connector restart-task franz kafka-pg-connector 0
--- services.tf
+++ services.tf
@@ -16,9 +16,9 @@ resource "aiven_kafka" "franz" {
 
   kafka_user_config {
     // Enables Kafka Connectors
-    kafka_connect = true
-    kafka_version = "3.8"
-    schema_registry                = true
+    kafka_connect   = true
+    kafka_version   = "3.8"
+    schema_registry = true
 
     kafka {
       group_max_session_timeout_ms = 70000
@@ -51,7 +51,7 @@ resource "aiven_kafka_connector" "kafka-os-connector" {
 
   config = {
     "topics"                         = aiven_kafka_topic.kafka-topic.topic_name
-    "topic.prefix" =  "prefix"
+    "topic.prefix"                   = "prefix"
     "connector.class"                = "io.aiven.kafka.connect.opensearch.OpensearchSinkConnector"
     "type.name"                      = "os-connector"
     "name"                           = var.kafka_connector_name
@@ -66,51 +66,64 @@ resource "aiven_kafka_connector" "kafka-os-connector" {
   }
 }
 
+locals {
+  schema_registry_uri = aiven_kafka.franz.kafka[0].schema_registry_uri
+
+  # Extract the user:password part from the URI
+  credentials_part = regex("https://([^@]+)@", local.schema_registry_uri)[0]
+
+  # Split into username and password
+  schema_registry_username = split(":", regexall("[^/]+", local.credentials_part)[0])[0]
+  schema_registry_password = split(":", regexall("[^/]+", local.credentials_part)[0])[1]
+}
 
 resource "aiven_kafka_connector" "kafka-pg-connector" {
   project        = aiven_kafka.franz.project
-  service_name   = "kafka-pg-connector"
+  service_name   = aiven_kafka.franz.service_name
   connector_name = "kafka-pg-connector"
 
   config = {
-    "name" ="kafka-pg-connector",
-    "connector.class" = "io.debezium.connector.postgresql.PostgresConnector",
-    "database.hostname"           = sensitive(aiven_pg.demo-pg.service_host)
-    "database.port"               = sensitive(aiven_pg.demo-pg.service_port)
-    "database.password"           = sensitive(aiven_pg.demo-pg.service_password)
-    "database.user"               = sensitive(aiven_pg.demo-pg.service_username)
-    "database.dbname"             = "defaultdb"
-    "database.ssl.mode" = "require",
-    "database.server.id" = "12345",
-    "plugin.name" = "pgoutput",
-    "publication.autocreate.mode" = "all_tables",
-    "topic.prefix" = "prefix",
-    "table.include.list" = "defaultdb.users",
-    "tasks.max" ="1",
-    "key.converter" = "io.confluent.connect.avro.AvroConverter",
-    "key.converter.schema.registry.url" = aiven_kafka.franz.service_uri,
-    "key.converter.basic.auth.credentials.source" = "USER_INFO",
-    "key.converter.schema.registry.basic.auth.user.info" = format("%s:%s",aiven_kafka.franz.service_username, aiven_kafka.franz.service_password),
-    "value.converter" = "io.confluent.connect.avro.AvroConverter",
-    "value.converter.schema.registry.url" = format("https://%s",aiven_kafka.franz.components[2].connection_uri),
-    "value.converter.basic.auth.credentials.source" = "USER_INFO",
-    "value.converter.schema.registry.basic.auth.user.info" = format("%s:%s",aiven_kafka.franz.service_username, aiven_kafka.franz.service_password),
-    "schema.history.internal.kafka.topic" = "topic",
-    "schema.history.internal.kafka.bootstrap.servers" = format("%s:%s", aiven_kafka.franz.service_host, aiven_kafka.franz.service_port),
-    "schema.history.internal.producer.security.protocol" = "SSL",
-    "schema.history.internal.producer.ssl.keystore.type" = "PKCS12",
-    "schema.history.internal.producer.ssl.keystore.location" = "/run/aiven/keys/public.keystore.p12",
-    "schema.history.internal.producer.ssl.keystore.password" = "password",
+    "name"                                                     = "kafka-pg-connector",
+    "topics"                                                   = aiven_kafka_topic.kafka-topic.topic_name
+    "connector.class"                                          = "io.debezium.connector.postgresql.PostgresConnector",
+    "database.hostname"                                        = sensitive(aiven_pg.demo-pg.service_host)
+    "database.port"                                            = sensitive(aiven_pg.demo-pg.service_port)
+    "database.password"                                        = sensitive(aiven_pg.demo-pg.service_password)
+    "database.user"                                            = sensitive(aiven_pg.demo-pg.service_username)
+    "database.dbname"                                          = "defaultdb"
+    "database.ssl.mode"                                        = "require",
+    "database.server.id"                                       = "12345",
+    "plugin.name"                                              = "pgoutput",
+    "publication.autocreate.mode"                              = "all_tables",
+    "topic.prefix"                                             = "prefix",
+    "table.include.list"                                       = "defaultdb.users",
+    "tasks.max"                                                = "1",
+    "include.schema.changes"                                   = "true"
+    /*
+    "key.converter"                                            = "io.confluent.connect.avro.AvroConverter",
+    "key.converter.schema.registry.url"                        = format("https://%s", resource.aiven_kafka.franz.components[2].connection_uri),
+    "key.converter.basic.auth.credentials.source"              = "USER_INFO",
+    "key.converter.schema.registry.basic.auth.user.info"       = format("%s:%s", local.schema_registry_username, local.schema_registry_password),
+    "value.converter"                                          = "io.confluent.connect.avro.AvroConverter",
+    "value.converter.schema.registry.url"                      = format("https://%s", aiven_kafka.franz.components[2].connection_uri),
+    "value.converter.basic.auth.credentials.source"            = "USER_INFO",
+    "value.converter.schema.registry.basic.auth.user.info"     = format("%s:%s", aiven_kafka.franz.service_username, aiven_kafka.franz.service_password),
+    "schema.history.internal.kafka.topic"                      = "topic",
+    "schema.history.internal.kafka.bootstrap.servers"          = format("%s:%s", aiven_kafka.franz.service_host, aiven_kafka.franz.service_port),
+    "schema.history.internal.producer.security.protocol"       = "SSL",
+    "schema.history.internal.producer.ssl.keystore.type"       = "PKCS12",
+    "schema.history.internal.producer.ssl.keystore.location"   = "/run/aiven/keys/public.keystore.p12",
+    "schema.history.internal.producer.ssl.keystore.password"   = "password",
     "schema.history.internal.producer.ssl.truststore.location" = "/run/aiven/keys/public.truststore.jks",
     "schema.history.internal.producer.ssl.truststore.password" = "password",
-    "schema.history.internal.producer.ssl.key.password" = "password",
-    "schema.history.internal.consumer.security.protocol" = "SSL",
-    "schema.history.internal.consumer.ssl.keystore.type" = "PKCS12",
-    "schema.history.internal.consumer.ssl.keystore.location" = "/run/aiven/keys/public.keystore.p12",
-    "schema.history.internal.consumer.ssl.keystore.password" = "password",
+    "schema.history.internal.producer.ssl.key.password"        = "password",
+    "schema.history.internal.consumer.security.protocol"       = "SSL",
+    "schema.history.internal.consumer.ssl.keystore.type"       = "PKCS12",
+    "schema.history.internal.consumer.ssl.keystore.location"   = "/run/aiven/keys/public.keystore.p12",
+    "schema.history.internal.consumer.ssl.keystore.password"   = "password",
     "schema.history.internal.consumer.ssl.truststore.location" = "/run/aiven/keys/public.truststore.jks",
     "schema.history.internal.consumer.ssl.truststore.password" = "password",
-    "schema.history.internal.consumer.ssl.key.password" = "password",
-    "include.schema.changes" = "true"
+    "schema.history.internal.consumer.ssl.key.password"        = "password",
+    */
   }
 }
